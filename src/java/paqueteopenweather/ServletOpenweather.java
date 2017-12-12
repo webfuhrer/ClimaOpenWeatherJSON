f94ba57e9c5b17ee40c468ce09690865639de158
webfuhrer <ataraxa@hotmail.com>
2017-12-12 09:45:28 +0100

    Primer commit

---
+++ src/java/paqueteopenweather/ServletOpenweather.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteopenweather;
+
+import conexion.ConexionURL;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author luis
+ */
+@WebServlet(name = "ServletOpenweather", urlPatterns = {"/ServletOpenweather"})
+public class ServletOpenweather extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       //Capturo la ciudad seleccionada
+       String ciudad_seleccionada="Barcelona";//Vendría del combo
+       String url="http://api.openweathermap.org/data/2.5/weather?q="+ciudad_seleccionada+"&appid=57703a7a9ab7b873a99116a3ea379748";
+       String json=ConexionURL.peticionWeb(url);
+       ObjetoClima oc=tratarJSON(json);//Supongo que ObjetoClima tiene 4 propiedades(t_min, t_max, sunrise, sunset).
+       request.setAttribute("clima",oc);
+       
+        System.out.println(json);
+       
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
